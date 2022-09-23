e5a1cc942ef617a62901f54d90b6cc1b41117eb2
sieun <supiya0908@naver.com>
2022-09-24 03:26:21 +0900

    Feat: Event 정보 조회(DB 연동)

--- src/main/java/app/learningtrip/apiserver/event/controller/EventController.java
+++ src/main/java/app/learningtrip/apiserver/event/controller/EventController.java
@@ -1,15 +1,26 @@
 package app.learningtrip.apiserver.event.controller;
 
 import app.learningtrip.apiserver.event.domain.Event;
+import app.learningtrip.apiserver.event.service.EventService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
     @GetMapping("/home/banner")
-    public Event eventList() {
-        return Event.builer()
-            .imgURL("https://i.ibb.co/PZcKz75/notice-A1-M7-E7-P2-X0-Q1-K5-Z4.jpg")
-            .build();
+    public ResponseEntity list(Model model) {
+        Optional<List<Event>> eventList = eventService.getList();
+        model. addAttribute("eventList", eventList);
+
+        return ResponseEntity.ok().body(model);
     }
 }
